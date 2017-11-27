a862d7a7bd8a3439e17a5df7d0525574b5b53b4f
jsychong <jasmine.chong@mail.mcgill.ca>
2017-11-27 17:32:05 -0500

    bug fixes in accordance w. user feedback

--- R/general_proc_utils.R
+++ R/general_proc_utils.R
@@ -285,14 +285,6 @@ RemoveMissingPercent <- function(mSetObj=NA, percent=perct){
   
   mSetObj <- .get.mSet(mSetObj);
   
-  if(!is.null(mSetObj$dataSet$norm)){
-    
-    int.mat <- mSetObj$dataSet$norm
-    minConc <- min(int.mat[int.mat>0], na.rm=T)/2;
-    good.inx <- apply(is.na(int.mat), 2, sum)/nrow(int.mat)<percent;
-    mSetObj$dataSet$preproc <- as.data.frame(int.mat[,good.inx]);
-    mSetObj$dataSet$norm <- as.data.frame(int.mat[,good.inx]);
-
   if(!.on.public.web){
     if(!is.null(mSetObj$dataSet$norm)){
       
@@ -316,7 +308,6 @@ RemoveMissingPercent <- function(mSetObj=NA, percent=perct){
       mSetObj$msgSet$replace.msg <- c(mSetObj$msgSet$replace.msg, paste(sum(!good.inx), "variables were removed for threshold", round(100*percent, 2), "percent."));
       return(.set.mSet(mSetObj));
     }
-    
   }else{
     
     int.mat <- mSetObj$dataSet$preproc
@@ -330,7 +321,6 @@ RemoveMissingPercent <- function(mSetObj=NA, percent=perct){
     
     return(.set.mSet(mSetObj));
     }
-  }
 }
 
 #'Data processing: Replace missing variables
@@ -411,14 +401,67 @@ ImputeVar <- function(mSetObj=NA, method="min"){
       mSetObj$dataSet$norm <- as.data.frame(new.mat);
       mSetObj$msgSet$replace.msg <- msg;
       return(.set.mSet(mSetObj));
+    }else{ #if normalization not first
+      int.mat <- mSetObj$dataSet$preproc
+      new.mat <- NULL;
+      msg <- mSetObj$msgSet$replace.msg;
+      
+      if(method=="exclude"){
+        good.inx<-apply(is.na(int.mat), 2, sum)==0
+        new.mat<-int.mat[,good.inx];
+        msg <- c(msg,"Variables with missing values were excluded.");
+      }else if(method=="min"){
+        minConc<-mSetObj$dataSet$minConc;
+        int.mat[int.mat==0 | is.na(int.mat)] <- minConc;
+        new.mat <- int.mat;
+        msg <- c(msg,"Variables with missing values were replaced with a small value.");
+      }else if (method=="colmin"){
+        new.mat<-apply(int.mat, 2, function(x){
+          if(sum(is.na(x))>0){
+            x[is.na(x)]<-min(x,na.rm=T)/2;
+          }
+          x;
+        });
+        msg <- c(msg,"Missing variables were replaced with the half of minimum values for each feature column.");
+      }else if (method=="mean"){
+        new.mat<-apply(int.mat, 2, function(x){
+          if(sum(is.na(x))>0){
+            x[is.na(x)]<-mean(x,na.rm=T);
+          }
+          x;
+        });
+        msg <- c(msg,"Missing variables were replaced with the mean value for each feature column.");
+      }else if (method == "median"){
+        new.mat<-apply(int.mat, 2, function(x){
+          if(sum(is.na(x))>0){
+            x[is.na(x)]<-median(x,na.rm=T);
+          }
+          x;
+        });
+        msg <- c(msg,"Missing variables were replaced with the median for each feature column.");
+      }else {
+        if(method == "knn"){
+          suppressMessages(library(impute));
+          #print("loading for KNN...");
+          new.mat<-t(impute.knn(t(int.mat))$data);
+        }else{
+          suppressMessages(library(pcaMethods));
+          if(method == "bpca"){
+            new.mat<-pca(int.mat, nPcs =5, method="bpca", center=T)@completeObs;
+          }else if(method == "ppca"){
+            new.mat<-pca(int.mat, nPcs =5, method="ppca", center=T)@completeObs;
+          }else if(method == "svdImpute"){
+            new.mat<-pca(int.mat, nPcs =5, method="svdImpute", center=T)@completeObs;
+          }
+        }
+        msg <- c(msg, paste("Missing variables were imputated using", toupper(method)));
+      }
+      mSetObj$dataSet$procr <- as.data.frame(new.mat);
+      mSetObj$msgSet$replace.msg <- msg;
+      print("Imp var")
+      return(.set.mSet(mSetObj));
     }
-
-    mSetObj$dataSet$norm <- as.data.frame(new.mat);
-    mSetObj$dataSet$procr <- as.data.frame(new.mat);
-    mSetObj$msgSet$replace.msg <- msg;
-    return(.set.mSet(mSetObj));
-
-  }else{
+  }else{ # if on web
     int.mat <- mSetObj$dataSet$preproc
     new.mat <- NULL;
     msg <- mSetObj$msgSet$replace.msg;
