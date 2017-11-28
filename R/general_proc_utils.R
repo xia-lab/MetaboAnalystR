0aa2f7052bce830b9989d8aa2ac934e6176cf573
jsychong <jasmine.chong@mail.mcgill.ca>
2017-11-28 11:58:28 -0500

    update to imputevar

--- R/general_proc_utils.R
+++ R/general_proc_utils.R
@@ -340,185 +340,75 @@ ImputeVar <- function(mSetObj=NA, method="min"){
   
   mSetObj <- .get.mSet(mSetObj);
   
-  if(!.on.public.web){
-    if(!is.null(mSetObj$dataSet$norm)){ # if normalization performed first
-      
-      int.mat <- mSetObj$dataSet$norm
-      new.mat <- NULL;
-      msg <- mSetObj$msgSet$replace.msg;
-      
-      if(method=="exclude"){
-        good.inx<-apply(is.na(int.mat), 2, sum)==0
-        new.mat<-int.mat[,good.inx];
-        msg <- c(msg,"Variables with missing values were excluded.");
-      }else if(method=="min"){
-        minConc<-min(int.mat[int.mat>0], na.rm=T)/2;
-        int.mat[int.mat==0 | is.na(int.mat)] <- minConc;
-        new.mat <- int.mat;
-        msg <- c(msg,"Variables with missing values were replaced with a small value.");
-      }else if (method=="colmin"){
-        new.mat<-apply(int.mat, 2, function(x){
-          if(sum(is.na(x))>0){
-            x[is.na(x)]<-min(x,na.rm=T)/2;
-          }
-          x;
-        });
-        msg <- c(msg,"Missing variables were replaced with the half of minimum values for each feature column.");
-      }else if (method=="mean"){
-        new.mat<-apply(int.mat, 2, function(x){
-          if(sum(is.na(x))>0){
-            x[is.na(x)]<-mean(x,na.rm=T);
-          }
-          x;
-        });
-        msg <- c(msg,"Missing variables were replaced with the mean value for each feature column.");
-      }else if (method == "median"){
-        new.mat<-apply(int.mat, 2, function(x){
-          if(sum(is.na(x))>0){
-            x[is.na(x)]<-median(x,na.rm=T);
-          }
-          x;
-        });
-        msg <- c(msg,"Missing variables were replaced with the median for each feature column.");
-      }else {
-        if(method == "knn"){
-          suppressMessages(library(impute));
-          #print("loading for KNN...");
-          new.mat<-t(impute.knn(t(int.mat))$data);
-        }else{
-          suppressMessages(library(pcaMethods));
-          if(method == "bpca"){
-            new.mat<-pca(int.mat, nPcs =5, method="bpca", center=T)@completeObs;
-          }else if(method == "ppca"){
-            new.mat<-pca(int.mat, nPcs =5, method="ppca", center=T)@completeObs;
-          }else if(method == "svdImpute"){
-            new.mat<-pca(int.mat, nPcs =5, method="svdImpute", center=T)@completeObs;
-          }
-        }
-        msg <- c(msg, paste("Missing variables were imputated using", toupper(method)));
-      }
-      mSetObj$dataSet$procr <- as.data.frame(new.mat);
-      mSetObj$dataSet$norm <- as.data.frame(new.mat);
-      mSetObj$msgSet$replace.msg <- msg;
-      return(.set.mSet(mSetObj));
-    }else{ #if normalization not first
-      int.mat <- mSetObj$dataSet$preproc
-      new.mat <- NULL;
-      msg <- mSetObj$msgSet$replace.msg;
-      
-      if(method=="exclude"){
-        good.inx<-apply(is.na(int.mat), 2, sum)==0
-        new.mat<-int.mat[,good.inx];
-        msg <- c(msg,"Variables with missing values were excluded.");
-      }else if(method=="min"){
-        minConc<-mSetObj$dataSet$minConc;
-        int.mat[int.mat==0 | is.na(int.mat)] <- minConc;
-        new.mat <- int.mat;
-        msg <- c(msg,"Variables with missing values were replaced with a small value.");
-      }else if (method=="colmin"){
-        new.mat<-apply(int.mat, 2, function(x){
-          if(sum(is.na(x))>0){
-            x[is.na(x)]<-min(x,na.rm=T)/2;
-          }
-          x;
-        });
-        msg <- c(msg,"Missing variables were replaced with the half of minimum values for each feature column.");
-      }else if (method=="mean"){
-        new.mat<-apply(int.mat, 2, function(x){
-          if(sum(is.na(x))>0){
-            x[is.na(x)]<-mean(x,na.rm=T);
-          }
-          x;
-        });
-        msg <- c(msg,"Missing variables were replaced with the mean value for each feature column.");
-      }else if (method == "median"){
-        new.mat<-apply(int.mat, 2, function(x){
-          if(sum(is.na(x))>0){
-            x[is.na(x)]<-median(x,na.rm=T);
-          }
-          x;
-        });
-        msg <- c(msg,"Missing variables were replaced with the median for each feature column.");
-      }else {
-        if(method == "knn"){
-          suppressMessages(library(impute));
-          #print("loading for KNN...");
-          new.mat<-t(impute.knn(t(int.mat))$data);
-        }else{
-          suppressMessages(library(pcaMethods));
-          if(method == "bpca"){
-            new.mat<-pca(int.mat, nPcs =5, method="bpca", center=T)@completeObs;
-          }else if(method == "ppca"){
-            new.mat<-pca(int.mat, nPcs =5, method="ppca", center=T)@completeObs;
-          }else if(method == "svdImpute"){
-            new.mat<-pca(int.mat, nPcs =5, method="svdImpute", center=T)@completeObs;
-          }
-        }
-        msg <- c(msg, paste("Missing variables were imputated using", toupper(method)));
-      }
-      mSetObj$dataSet$procr <- as.data.frame(new.mat);
-      mSetObj$msgSet$replace.msg <- msg;
-      print("Imp var")
-      return(.set.mSet(mSetObj));
-    }
-  }else{ # if on web
+  if(!is.null(mSetObj$dataSet$norm)){ # if normalization performed first
+    int.mat <- mSetObj$dataSet$norm
+  }else{
     int.mat <- mSetObj$dataSet$preproc
-    new.mat <- NULL;
-    msg <- mSetObj$msgSet$replace.msg;
-    
-    if(method=="exclude"){
-      good.inx<-apply(is.na(int.mat), 2, sum)==0
-      new.mat<-int.mat[,good.inx];
-      msg <- c(msg,"Variables with missing values were excluded.");
-    }else if(method=="min"){
-      minConc<-mSetObj$dataSet$minConc;
-      int.mat[int.mat==0 | is.na(int.mat)] <- minConc;
-      new.mat <- int.mat;
-      msg <- c(msg,"Variables with missing values were replaced with a small value.");
-    }else if (method=="colmin"){
-      new.mat<-apply(int.mat, 2, function(x){
-        if(sum(is.na(x))>0){
-          x[is.na(x)]<-min(x,na.rm=T)/2;
-        }
-        x;
-      });
-      msg <- c(msg,"Missing variables were replaced with the half of minimum values for each feature column.");
-    }else if (method=="mean"){
-      new.mat<-apply(int.mat, 2, function(x){
-        if(sum(is.na(x))>0){
-          x[is.na(x)]<-mean(x,na.rm=T);
-        }
-        x;
-      });
-      msg <- c(msg,"Missing variables were replaced with the mean value for each feature column.");
-    }else if (method == "median"){
-      new.mat<-apply(int.mat, 2, function(x){
-        if(sum(is.na(x))>0){
-          x[is.na(x)]<-median(x,na.rm=T);
-        }
-        x;
-      });
-      msg <- c(msg,"Missing variables were replaced with the median for each feature column.");
-    }else {
-      if(method == "knn"){
-        suppressMessages(library(impute));
-        #print("loading for KNN...");
-        new.mat<-t(impute.knn(t(int.mat))$data);
-      }else{
-        suppressMessages(library(pcaMethods));
-        if(method == "bpca"){
-          new.mat<-pca(int.mat, nPcs =5, method="bpca", center=T)@completeObs;
-        }else if(method == "ppca"){
-          new.mat<-pca(int.mat, nPcs =5, method="ppca", center=T)@completeObs;
-        }else if(method == "svdImpute"){
-          new.mat<-pca(int.mat, nPcs =5, method="svdImpute", center=T)@completeObs;
-        }
+  }
+  
+  new.mat <- NULL;
+  msg <- mSetObj$msgSet$replace.msg;
+  
+  if(method=="exclude"){
+    good.inx<-apply(is.na(int.mat), 2, sum)==0
+    new.mat<-int.mat[,good.inx];
+    msg <- c(msg,"Variables with missing values were excluded.");
+  }else if(method=="min"){
+    minConc<-min(int.mat[int.mat>0], na.rm=T)/2;
+    int.mat[int.mat==0 | is.na(int.mat)] <- minConc;
+    new.mat <- int.mat;
+    msg <- c(msg,"Variables with missing values were replaced with a small value.");
+  }else if (method=="colmin"){
+    new.mat<-apply(int.mat, 2, function(x){
+      if(sum(is.na(x))>0){
+        x[is.na(x)]<-min(x,na.rm=T)/2;
       }
-      msg <- c(msg, paste("Missing variables were imputated using", toupper(method)));
+      x;
+    });
+    msg <- c(msg,"Missing variables were replaced with the half of minimum values for each feature column.");
+  }else if (method=="mean"){
+    new.mat<-apply(int.mat, 2, function(x){
+      if(sum(is.na(x))>0){
+        x[is.na(x)]<-mean(x,na.rm=T);
+      }
+      x;
+    });
+    msg <- c(msg,"Missing variables were replaced with the mean value for each feature column.");
+  }else if (method == "median"){
+    new.mat<-apply(int.mat, 2, function(x){
+      if(sum(is.na(x))>0){
+        x[is.na(x)]<-median(x,na.rm=T);
+      }
+      x;
+    });
+    msg <- c(msg,"Missing variables were replaced with the median for each feature column.");
+  }else{
+    if(method == "knn"){
+      suppressMessages(library(impute));
+      #print("loading for KNN...");
+      new.mat<-t(impute.knn(t(int.mat))$data);
+    }else{
+      suppressMessages(library(pcaMethods));
+      if(method == "bpca"){
+        new.mat<-pca(int.mat, nPcs =5, method="bpca", center=T)@completeObs;
+      }else if(method == "ppca"){
+        new.mat<-pca(int.mat, nPcs =5, method="ppca", center=T)@completeObs;
+      }else if(method == "svdImpute"){
+        new.mat<-pca(int.mat, nPcs =5, method="svdImpute", center=T)@completeObs;
+      }
     }
+    msg <- c(msg, paste("Missing variables were imputated using", toupper(method)));
+  }
+  
+  if(!is.null(mSetObj$dataSet$norm)){
     mSetObj$dataSet$procr <- as.data.frame(new.mat);
+    mSetObj$dataSet$norm <- as.data.frame(new.mat);
     mSetObj$msgSet$replace.msg <- msg;
     return(.set.mSet(mSetObj));
+  }else{
+    mSetObj$dataSet$procr <- as.data.frame(new.mat);
+    mSetObj$msgSet$replace.msg <- msg;
+    return(.set.mSet(mSetObj))
   }
 }
 
