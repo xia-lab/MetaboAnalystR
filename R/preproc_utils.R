46f802135b7b4ffe94dafea7f6059975bc11564a
jsychong <jasmine.chong@mail.mcgill.ca>
2019-05-02 15:18:14 -0400

    update doc to fix formatpeaklist manual

--- R/preproc_utils.R
+++ R/preproc_utils.R
@@ -482,7 +482,7 @@ PerformPeakAnnotation <- function(xset, annParams){
 #' positive ion more and [M-H]- for negative ion mode. By default it is set to false.
 #' @param missPercent Numeric, specify the threshold to remove features
 #' missing in X\% of samples. For instance, 0.5 specifies to remove features
-#' that are missing from 50% of all samples per group.
+#' that are missing from 50\% of all samples per group.
 #' @author Jasmine Chong \email{jasmine.chong@mail.mcgill.ca},
 #' Mai Yamamoto \email{yamamoto.mai@mail.mcgill.ca}, and Jeff Xia \email{jeff.xia@mcgill.ca}
 #' McGill University, Canada
