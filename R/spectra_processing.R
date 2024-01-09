a7401f29f6b8d9decad35e53beb2e34a8b4fc085
Zhiqiang <zhiqiang.pang@mail.mcgill.ca>
2024-01-09 13:55:13 -0500

    ms2 spectra processing update

--- R/spectra_processing.R
+++ R/spectra_processing.R
@@ -36,7 +36,11 @@ CreateRawRscript <- function(guestName, planString, planString2, rawfilenms.vec)
   }
   
   ## create algorithm marker
-  write.table("optilcms", file = "ms1_algorithm.txt", quote = F, sep = "", col.names = F, row.names = F)
+  if(grepl("ROIExtraction", planString)){
+    write.table("centWave_auto", file = "ms1_algorithm.txt", quote = F, sep = "", col.names = F, row.names = F)
+  } else {
+    write.table("centWave_manual", file = "ms1_algorithm.txt", quote = F, sep = "", col.names = F, row.names = F)
+  }
   
   ## Prepare Configuration script for slurm running
   conf_inf <- paste0("#!/bin/bash\n#\n#SBATCH --job-name=Spectral_Processing\n#\n#SBATCH --ntasks=1\n#SBATCH --time=720:00\n#SBATCH --mem-per-cpu=5G\n#SBATCH --cpus-per-task=2\n#SBATCH --output=", users.path, "/metaboanalyst_spec_proc.txt\n")
@@ -783,6 +787,10 @@ retrieveAlgorithmInfo <- function(){
   plantext <- readLines("ms1_algorithm.txt")
   if(grepl("asari", plantext)){
     return("asari")
+  } else if(grepl("centWave_auto", plantext)){
+    return("centWave_auto")
+  } else if(grepl("centWave_manual", plantext)){
+    return("centWave_manual")
   } else {
     return("optilcms")
   }
