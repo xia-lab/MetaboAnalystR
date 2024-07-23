82bf153383aa7365e23b3646282baa50b085427f
xialab <xialab@localhost>
2024-07-23 11:56:35 -0400

    Volcano fix

--- R/util_volcano.R
+++ R/util_volcano.R
@@ -7,7 +7,7 @@
 # if labelNum = -1 --> all sig labels
 # if labelNum = 0 --> no labels
 # else top labels #
-my.plot.volcano <- function(mSetObj=NA, imgName="NA", plotLbl=T, plotTheme=0, format="png", dpi=72, width=NA,labelNum=5,  interactive=F){
+my.plot.volcano <- function(mSetObj=NA, imgName="NA", plotLbl=T, plotTheme=0, format="png", dpi=72, width=NA, labelNum=5, interactive=F){
   #save.image("volc.RData");
   mSetObj <- .get.mSet(mSetObj);
   
@@ -35,11 +35,6 @@ my.plot.volcano <- function(mSetObj=NA, imgName="NA", plotLbl=T, plotTheme=0, fo
   de$Status[vcn$inx.p & vcn$inx.up] <- "UP";
   de$Status[vcn$inx.p & vcn$inx.down] <- "DOWN";
   de$Status <- as.factor(de$Status);
-  
-  mycols <- levels(de$Status);
-  mycols[mycols=="UP"] <- "firebrick";
-  mycols[mycols=="DOWN"] <- "cornflowerblue";
-  mycols[mycols=="Non-SIG"] <- "grey";
 
   # Calculate percentile ranks (0-100 scale for easier interpretation)
   de$p.rank <- rank(de$p.log) / length(de$p.log) * 100  # Higher p-values (lower significance) get lower rank
@@ -57,36 +52,34 @@ my.plot.volcano <- function(mSetObj=NA, imgName="NA", plotLbl=T, plotTheme=0, fo
     de$label[imp.inx] <- rownames(de)[imp.inx];
   }else{
     de$label[c(1:labelNum)] <- rownames(de)[c(1:labelNum)];
-    
   }
 
   require(ggplot2);
   require(scales);
   
-  de$size <- rescale(de$p.log, to = c(1.5, 4))
-  
-  # Create a new variable for gradient coloring based on significance
-  de$FoldChange <- ifelse(de$Status != "Non-SIG", de$fc.log, NA)
-  
-  # Get the range of log fold changes for significant points
-  sig_fc_range <- range(de$fc.log[de$Status != "Non-SIG"], na.rm = TRUE)
-  
-  # Create the ggplot
-  p <- ggplot(data = de, aes(x = fc.log, y = p.log, label = label, Fold.Change = Fold.Change, P.Value= P.Value)) +
-    geom_point(aes( size = size, color =FoldChange, fill=FoldChange) , shape = 21, stroke = 0.5) +
-    scale_color_gradient2(low = "black", mid = "black", high = "black", midpoint = 0,
-                          limits = sig_fc_range, space = "Lab", na.value = "black", guide="none") +
-    scale_fill_gradient2(low = "blue", mid = "grey", high = "red", midpoint = 0,
-                          limits = sig_fc_range, space = "Lab", na.value = "grey", guide = "colourbar") +
-    scale_size_continuous(range = c(1, 4),guide="none") +
-    geom_vline(xintercept = c(vcn$min.xthresh, vcn$max.xthresh), linetype = "dashed", color = "black") +
-    geom_hline(yintercept = vcn$thresh.y, linetype = "dashed", color = "black") +
-    labs(x = "log2(FC)", y = "-log10(p-value)") +
-    theme_minimal() +
-    theme(legend.position = "right")
-  
-  # Print the plot
-  
+  # size on p values, further boost for sig
+  de$size <- rescale(de$p.log, to = c(1.0, 4))
+  de$size[imp.inx] <- de$size[imp.inx] + 0.5;
+
+  # Create a new variable for gradient coloring based on ALL nodes, as this is continuous, threshold is for plot only
+
+    de$FoldChange <- de$fc.log;
+    fc_range <- range(de$fc.log); 
+    
+    # Create the ggplot
+    p <- ggplot(data = de, aes(x = fc.log, y = p.log, label = label, Fold.Change = Fold.Change, P.Value= P.Value)) +
+        geom_point(aes(size = size, color =FoldChange, fill=FoldChange), shape = 21, stroke = 0.3) +
+        scale_color_gradient2(low = "black", mid = "black", high = "black", midpoint = 0,
+                          limits = fc_range, space = "Lab", na.value = "black", guide="none") +
+        scale_fill_gradient2(low = "blue", mid = "grey", high = "red", midpoint = 0, name = "Log2(FC)",
+                          limits = fc_range, space = "Lab", na.value = "grey", guide = "colourbar") +
+        scale_size_continuous(name = "P-value", range = c(1, 6), limits = c(0, 8), breaks = c(0, 1, 1.3, 2, 3), labels = c("1.0", "0.1", "0.05", "0.01", "0.001")) +
+        geom_vline(xintercept = c(vcn$min.xthresh, vcn$max.xthresh), linetype = "dashed", color = "black") +
+        geom_hline(yintercept = vcn$thresh.y, linetype = "dashed", color = "black") +
+        labs(x = "log2(FC)", y = "-log10(p-value)") +
+        theme_minimal() +
+        theme(legend.position = "right");
+
   if(plotLbl){
     p <- p +  ggrepel::geom_text_repel();
   }
