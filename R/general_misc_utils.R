879481a1bdadde2b515917504cefd61052c32f66
Xia Lab <xialab@Xias-MacBook-Pro.local>
2026-08-26 17:46:12 -0400

    Fix "Interquantile Range" typo -> "Interquartile Range"
    The low-variance IQR filter names its own method wrong in the filter
    message shown to users (report/slides/dashboard). User-caught 2026-08-26.

--- R/general_misc_utils.R
+++ R/general_misc_utils.R
@@ -1563,7 +1563,7 @@ PerformFeatureFilter <- function(int.mat, filter, filter.cutoff, anal.type){
       nm <- "median";
     }else{ # iqr
       filter.val <- apply(int.mat, 2, IQR, na.rm=T);
-      nm <- "Interquantile Range";
+      nm <- "Interquartile Range";
   }
 
     # get the rank of the filtered variables
